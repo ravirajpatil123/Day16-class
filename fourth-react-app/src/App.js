5369ca7afc78aae59785991b5b45487ba0bfa7ad
Raviraj patil <ravirajpatil298@gmail.com>
2023-06-15 09:11:40 +0530

    static

---
+++ fourth-react-app/src/App.js
@@ -0,0 +1,18 @@
+function App(){
+  return(
+    <>
+    <h1>Props Demo</h1>
+    <MessageDemo/>
+    <MessageDemo/>
+    </>
+  )
+}
+
+function MessageDemo(){
+  return (
+    <>
+     <h1>hello message</h1>
+    </>
+  );
+}
+export default App;
