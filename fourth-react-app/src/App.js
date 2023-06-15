2ce2a6e6965aa6f877df018132c93769f48efeba
Raviraj patil <ravirajpatil298@gmail.com>
2023-06-15 19:00:27 +0530

    update

--- fourth-react-app/src/App.js
+++ fourth-react-app/src/App.js
@@ -9,19 +9,22 @@ function App() {
 }
 
 function MyTodo() {
-  let [todo, setTodo] = useState({ task: "", desc: "" });
-  let handleChangeTaskAction = (e) =>{
-      //console.log(e.target);
+  let [todo, setTodo] = useState({ task: "", description: "" });
+  let handleChangeTaskAction = (e) => {
+    //console.log(e.target);
 
-      let newTodo ={...todo,task: e.target.value};
-      setTodo(newTodo);
-
+    let newTodo = { ...todo, task: e.target.value };
+    setTodo(newTodo);
+  };
+  let handleChangeDescriptionAction=(e) => {
+    let newTodo = { ...todo, description: e.target.value };
+    setTodo(newTodo);
   }
 
-
-  let addTodoAction=() =>{
-    alert(todo.task);
-  }
+  let addTodoAction = () => {
+   // alert(todo.task + todo.description);
+   console.log(todo)
+  };
 
   return (
     <>
@@ -32,6 +35,16 @@ function MyTodo() {
         value={todo.task}
         onChange={handleChangeTaskAction}
       />
+
+      <textarea
+        className="form-control"
+       
+        cols="30"
+        rows="3"
+        placeholder="enter desciption"
+        value={todo.description}
+        onChange={handleChangeDescriptionAction}
+      ></textarea>
 
       <input type="button" value="Add Todo" onClick={addTodoAction} />
     </>
