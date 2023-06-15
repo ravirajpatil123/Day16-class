35b48487c170d78b9df7241121b69c06bdd0c40c
Raviraj patil <ravirajpatil298@gmail.com>
2023-06-15 11:23:26 +0530

    userinput

--- fourth-react-app/src/App.js
+++ fourth-react-app/src/App.js
@@ -1,37 +1,33 @@
+import {useState} from "react";
 function App(){
   return(
     <>
-    <h1>Props Demo</h1>
+    <h1>Stateful List</h1>
     <ListDemo/>
     </>
   )
 }
 
 function ListDemo(){
-let data = "hello Ravi";
-let list =["delhi","pune","mumbai"];
+  let [list,setList]= useState(["Delhi"]);
 
-console.log(list);
-  return (
-    <>
-      <h1>{data}</h1>
-      {list.forEach((item) => item)}
-      
-      {list.map((item) =>
-      {
-      <h1>hello{item}</h1>
-      })}
-    </>
-  )
+  let addItemAction = () => {
+    let inputElement=document.querySelector("#id1");
+    let inputValue=inputElement.value
+    let newList = [...list,inputValue];
+    setList(newList);
+  };
+return(
+  <>
+  <input type="text" id="id1" placeholder="whatsup..." />
+  <input type="button" value="Add New Item" onClick={addItemAction} />
+  {list.map((item)=>(
+    <h1>{item}</h1>
+  ))}
+  </>
+);
+  
 }
 
-// function MessageDemo({username,email}){
-// // console.log("I am inside messagedemo",props)
 
-//   return (
-//     <>
-//      <h1>hello {username}{email}</h1>
-//     </>
-//   );
-// }
 export default App;
