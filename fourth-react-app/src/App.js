120083efc29cd24bd09e8ffafdc7f5a2bd8c8be9
Raviraj patil <ravirajpatil298@gmail.com>
2023-06-15 09:30:46 +0530

    function with parameter

--- fourth-react-app/src/App.js
+++ fourth-react-app/src/App.js
@@ -2,16 +2,19 @@ function App(){
   return(
     <>
     <h1>Props Demo</h1>
-    <MessageDemo/>
-    <MessageDemo/>
+    <MessageDemo username="Ravi"/>
+    <MessageDemo username="Sanket"/>
+    <MessageDemo username="Raj"/>
     </>
   )
 }
 
-function MessageDemo(){
+function MessageDemo(props){
+// console.log("I am inside messagedemo",props)
+
   return (
     <>
-     <h1>hello message</h1>
+     <h1>hello {props.username}</h1>
     </>
   );
 }
