751f76dd5186c0026a66275b5a7526455bf5fb5d
Raviraj patil <ravirajpatil298@gmail.com>
2023-06-15 10:01:35 +0530

    list

--- fourth-react-app/src/App.js
+++ fourth-react-app/src/App.js
@@ -2,20 +2,32 @@ function App(){
   return(
     <>
     <h1>Props Demo</h1>
-    <MessageDemo username="Ravi" email="ravi@gmail.com"/>
-    <MessageDemo username="Sanket" email="sanket@gmail.com"/>
-    <MessageDemo username="Raj" email="raj@gmail.com"/>
+    <ListDemo/>
     </>
   )
 }
 
-function MessageDemo({username,email}){
-// console.log("I am inside messagedemo",props)
+function ListDemo(){
+let data = "hello Ravi";
+let list =["delhi","pune","mumbai"];
 
+console.log(list);
   return (
     <>
-     <h1>hello {username}{email}</h1>
+      <h1>{data}</h1>
+      {list.forEach((item) => item)}
+      {list.map((item) => item)}
     </>
-  );
+  )
 }
+
+// function MessageDemo({username,email}){
+// // console.log("I am inside messagedemo",props)
+
+//   return (
+//     <>
+//      <h1>hello {username}{email}</h1>
+//     </>
+//   );
+// }
 export default App;
